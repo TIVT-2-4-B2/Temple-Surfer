7599ee5144f620488e20bd79b94dc390b7857a9b
luuk <lah.vanberkel@student.avans.nl>
2022-05-18 13:03:38 +0200

    Fixed gitkraken mistakes

--- TempleSurfer/PlayerComponent.cpp
+++ TempleSurfer/PlayerComponent.cpp
@@ -15,16 +15,13 @@ PlayerComponent::~PlayerComponent()
 }
 
 void PlayerComponent::update(float elapsedTime)
-{
-
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		gameObject->position.z -= elapsedTime * speed;
+{	
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		gameObject->position.z += elapsedTime * speed;
+		moveCenter();
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		gameObject->position.x -= elapsedTime * speed;
+		moveLeft();
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		gameObject->position.x += elapsedTime * speed;
+		moveRight();
 }
 
 void PlayerComponent::moveLeft()
